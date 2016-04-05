0c986c05f5e1364bb7ef1b41a9ae5492dd0eb07b
Marek Mystkowski <marekmystkowski@vp.pl>
2016-04-05 22:04:08 +0200

    Kompletna aplikacja

--- www/wybory/views.py
+++ www/wybory/views.py
@@ -10,12 +10,15 @@ def index(request):
     przedziały = [(0, 100), (101, 200), (201, 1000)]
 
     #kolory do tabeli:
+    kolor_dla_równych = "#f2f1f6"
     kolory= [('#e7deeb', '#fdeae2'), ('#d9cce0', '#fcded3'), ('#c6b2d1', '#fbcdbc'), ('#a180b2', '#f8ac90'), ('#8e67a3', '#f79c7a'),
              ('#7b4d94', '#f58b64'), ('#693485', '#f47b4e'), ('#5a2079', '#f26a38'), ('#4d0e6e', '#f15a22'), ('#410360', '#f54200')]
 
     #nazwa województwa a id na mapie
-    mapa_pola = {"Dolnośląskie":"land1", "Kujawsko-Pomorskie":"land2", "Łódzkie":"land3", "Małopolskie":"land6", "Podlaskie":"land10"}
-
+    mapa_pola = {"Dolnośląskie":"land1", "Kujawsko-Pomorskie":"land2", "Łódzkie":"land3", "Lubelskie":"land4", "Lubuskie":"land5",
+                 "Małopolskie":"land6", "Mazowieckie":"land7", "Opolskie":"land8", "Podkarpackie":"land9", "Podlaskie":"land10",
+                 "Pomorskie":"land11", "Śląskie":"land12", "Świętokrzyskie":"land13", "Warmińsko-Mazurskie":"land14",
+                 "Wielkopolskie":"land15", "Zachodniopomorskie":"land16"}
 
     #funkcje i klasy pomocnicze:
     def add2(a, b):
@@ -24,7 +27,7 @@ def index(request):
     def add4(a, b):
         return a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]
 
-    def procrnt(a, b):
+    def procent(a, b):
         if b == 0:
             return '0/0'
         else:
@@ -47,8 +50,8 @@ def index(request):
         def __init__(self, nazwa, na_pierwszego, na_drugiego):
             self.nazwa, self.licz_na_pi, self.licz_na_dr = nazwa, na_pierwszego, na_drugiego
             self.licz_wazny = na_pierwszego + na_drugiego
-            self.proc_na_pi = procrnt(na_pierwszego, self.licz_wazny)
-            self.proc_na_dr = procrnt(na_drugiego, self.licz_wazny)
+            self.proc_na_pi = procent(na_pierwszego, self.licz_wazny)
+            self.proc_na_dr = procent(na_drugiego, self.licz_wazny)
 
 
     #dane ogólne do wypisania:
@@ -62,22 +65,19 @@ def index(request):
 
 
     #tabela po województwach:
-    tabela_wejewodztw = map(lambda t: SetDateToTabe(t[0], t[1][0], t[1][1]),
+    tabela_wejewodztw = []
+    for elem in map(lambda t: SetDateToTabe(t[0], t[1][0], t[1][1]),
                          [(wojwództwo.nazwa, glosy_na(wojwództwo=wojwództwo)) for wojwództwo in Województwo.objects.all()]
-                             )
-    tabela_wejewodztw2 = map(lambda t: SetDateToTabe(t[0], t[1][0], t[1][1]),
-                        [(wojwództwo.nazwa, glosy_na(wojwództwo=wojwództwo)) for wojwództwo in Województwo.objects.all()]
-                            )
-    tabela_wejewodztw3 = map(lambda t: SetDateToTabe(t[0], t[1][0], t[1][1]),
-                             [(wojwództwo.nazwa, glosy_na(wojwództwo=wojwództwo)) for wojwództwo in
-                              Województwo.objects.all()]
-                             )
+                             ):
+        tabela_wejewodztw.append(elem)
 
 
     #tabela po kategorjach:
-    po_kategorjach = map(lambda t : SetDateToTabe(t[0],t[1][0],t[1][1]),
+    po_kategorjach =  []
+    for elem in map(lambda t : SetDateToTabe(t[0],t[1][0],t[1][1]),
                          [(rodzaj.rodzaj, glosy_na(rodzaj=rodzaj))for rodzaj in RodzajGminy.objects.all()]
-                         )
+                         ):
+        po_kategorjach.append(elem)
 
 
     #tabela po rozmiarach:
@@ -98,13 +98,12 @@ def index(request):
 
     # mapa wraz z tabelką:
     minimalny_procent = 50
-    for iter in tabela_wejewodztw2:
+    for iter in tabela_wejewodztw:
         if iter.licz_wazny != 0:
             minimalny_procent = min(minimalny_procent,
                                     100 * min(iter.licz_na_pi, iter.licz_na_dr) / iter.licz_wazny)
     if minimalny_procent == 50: minimalny_procent = 40
     skok = (50 - minimalny_procent) / len(kolory)
-    tab_kolorow = []
 
     class EleTabKol:
         def __init__(self, przedzial, kolorA, kolorB):
@@ -115,16 +114,23 @@ def index(request):
             self.land, self.color,  = land, kolor
 
     tmp = 50
+    tab_kolorow = [EleTabKol("50", kolor_dla_równych, kolor_dla_równych)]
     for kolor in kolory:
         tmp += skok
-        tab_kolorow.append(EleTabKol(procrnt(tmp, 100), kolor[0], kolor[1]))
+        tab_kolorow.append(EleTabKol(procent(tmp, 100), kolor[0], kolor[1]))
 
     kolory_na_mapie = []
-    for iter in tabela_wejewodztw3:
+    def kolor(na_pierwszego):
+        na_pierwszego = float(na_pierwszego)
+        ile = int((max(na_pierwszego, 100 - na_pierwszego) - 50.00001)/skok)
+        if na_pierwszego == 50: return kolor_dla_równych
+        if na_pierwszego < 50: return tab_kolorow[ile + 1].kolorB
+        return tab_kolorow[ile + 1].kolorA
+
+    for iter in tabela_wejewodztw:
         try:
-            if mapa_pola[iter.nazwa]:
-                kolory_na_mapie.append(EleTabKolNaMap(mapa_pola[iter.nazwa], tab_kolorow[4].kolorB))
-        except:pass
+            kolory_na_mapie.append(EleTabKolNaMap(mapa_pola[iter.nazwa], kolor(iter.proc_na_pi)))
+        except: pass
 
 
 
@@ -138,9 +144,9 @@ def index(request):
         'licz_na_pi' : licz_na_pi,
         'licz_na_dr' : licz_na_dr,
         'licz_wazny' : licz_na_pi + licz_na_dr,
-        'frekwencja' : procrnt(liczba_głosów_oddanych, liczba_uprawnionych),
-        'proc_na_pi' : procrnt(licz_na_pi, licz_na_pi + licz_na_dr),
-        'proc_na_dr': procrnt(licz_na_dr, licz_na_pi + licz_na_dr),
+        'frekwencja' : procent(liczba_głosów_oddanych, liczba_uprawnionych),
+        'proc_na_pi' : procent(licz_na_pi, licz_na_pi + licz_na_dr),
+        'proc_na_dr': procent(licz_na_dr, licz_na_pi + licz_na_dr),
         'tabela_wejewodztw' : tabela_wejewodztw,
         'po_kategorjach' : po_kategorjach,
         'po_rozmiarze' : po_rozmiarze,
